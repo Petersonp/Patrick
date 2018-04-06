09614988416e1b46c1fe9fbdd3b1e991d71edeb1
Patrick <Patrick@172.21.52.144>
2018-04-06 12:15:42 +0800

    v.2

--- src/todolist/Model.java
+++ src/todolist/Model.java
@@ -19,6 +19,20 @@ public class Model {
 
     Model() {
 //constructor
+String lines[] = helper.readTasks();
+taskCount = lines.length ;
+
+for (int i = 0; i< taskCount;i++){
+    
+    tasks[i] = new Task();
+    //loops through and creates new task out of every line
+    //
+    String taskLine[] = lines[i].split("#");
+    tasks[i].setTitle(taskLine[0]);
+    tasks[i].setDescription(taskLine[1]);
+    
+}
+
     }
 
     void add(String description, String title) {
@@ -27,24 +41,40 @@ public class Model {
         task.setTitle(title);
         tasks[taskCount] = task;
         taskCount++;
+        
+        helper.writeTasks(getTasks());
     }
 
     void remove(int index) {
+        tasks[index].setDescription("");
+        tasks[index].setTitle("");
         for (int i = index; i < taskCount; i++) {
-            tasks[i] = tasks[i+1];
+            tasks[i] = tasks[i + 1];
         }
+        taskCount--;
+        helper.writeTasks(getTasks());
+        
     }
 
     void edit(int index, String description, String title) {
         tasks[index].setDescription(description);
         tasks[index].setTitle(title);
+        helper.writeTasks(getTasks());
     }
-    String[] getTasks(){
+
+    String[] getTasks() {
         String[] tmpTasks = new String[taskCount];
-        for(int n = 0;n<taskCount;n++){
+        for (int n = 0; n < taskCount; n++) {
             tmpTasks[n] = tasks[n].toString();
-            
+
         }
         return tmpTasks;
+    }
+
+    String[] getTaskValues(int index) {
+        String[] values = new String[2];
+        values[0] = tasks[index].getTitle();
+        values[1] = tasks[index].getDescription();
+        return values;
     }
 }
